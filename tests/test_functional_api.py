b25ec456267bd259728ee04831d7513c0a0ae217
arthur120garcia <arthur120garcia@gmail.com>
2023-06-26 06:55:17 -0300

    Functional test

---
+++ tests/test_functional_api.py
@@ -0,0 +1,21 @@
+from fastapi.testclient import TestClient
+from beerlog.api import api
+
+client = TestClient(api)
+
+
+def test_create_beer_via_api():
+    response = client.post(
+        "/beers",
+        json={
+            "name": "Test",
+            "style": "Testing",
+            "flavor": 1,
+            "image": 5,
+            "cost": 8
+        }
+    )
+    assert response.status_code == 200
+    result = response.json()
+    assert result["name"] == "Test"
+    assert result["id"] == 1
